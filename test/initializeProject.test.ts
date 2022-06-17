5e0a8e4c1bab8b3f9afe341a3b20435a54d049d6
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-17 22:39:35 +0900

    fixed test

--- test/initializeProject.test.ts
+++ test/initializeProject.test.ts
@@ -14,9 +14,9 @@ describe("checks that the app creates the necessary files to start", () => {
 		expect(fs.existsSync("queue.json")).to.be.not.ok
 		expect(fs.existsSync("failedDownloads.json")).to.not.be.ok
 
-		Promise.resolve(initializeProject()).then(() => {
-			expect(fs.existsSync("queue.json")).to.be.ok
-			expect(fs.existsSync("failedDownloads.json")).to.be.ok
-		})
+		await initializeProject()
+
+		expect(fs.existsSync("queue.json")).to.be.ok
+		expect(fs.existsSync("failedDownloads.json")).to.be.ok
 	})
 })
