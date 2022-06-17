8f0f2bde3fe73220b4fa4f63557fbb1395ef4f2d
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-18 02:24:52 +0900

    fixed this test, for good this time (at least I think so...)

--- test/initializeProject.test.ts
+++ test/initializeProject.test.ts
@@ -7,22 +7,32 @@ import initializeProject from "../src/initializeProject"
 describe("checks that the app creates the necessary files to start", () => {
 	it("creates .json files", async () => {
 		if (
-			(await fs.existsSync("./queue.initializeProject.json")) ||
-			(await fs.existsSync("failedDownloads.initializeProject.json"))
+			fs.existsSync("./queue.initializeProject.json") ||
+			fs.existsSync("failedDownloads.initializeProject.json")
 		) {
-			await fs.unlinkSync("./queue.initializeProject.json")
-			await fs.unlinkSync("./failedDownloads.initializeProject.json")
+			fs.unlinkSync("./queue.initializeProject.json")
+			fs.unlinkSync("./failedDownloads.initializeProject.json")
 		}
 
-		expect(await fs.existsSync("./queue.initializeProject.json")).to.be.not.ok
-		expect(await fs.existsSync("./failedDownloads.initializeProject.json")).to.not.be.ok
+		expect(fs.existsSync("./queue.initializeProject.json")).to.be.not.ok
+		expect(fs.existsSync("./failedDownloads.initializeProject.json")).to.not.be.ok
 
-		await initializeProject()
+		initializeProject().then(async () => {
+			expect(fs.existsSync("./queue.initializeProject.json")).to.be.ok
+			expect(fs.existsSync("./failedDownloads.initializeProject.json")).to.be.ok
 
-		expect(await fs.existsSync("./queue.initializeProject.json")).to.be.ok
-		expect(await fs.existsSync("./failedDownloads.initializeProject.json")).to.be.ok
+			fs.unlinkSync("./queue.initializeProject.json")
+			fs.unlinkSync("./failedDownloads.initializeProject.json")
+		})
 
-		await fs.unlinkSync("./queue.initializeProject.json")
-		await fs.unlinkSync("./failedDownloads.initializeProject.json")
+		after(() => {
+			if (
+				fs.existsSync("./queue.initializeProject.json") ||
+				fs.existsSync("failedDownloads.initializeProject.json")
+			) {
+				fs.unlinkSync("./queue.initializeProject.json")
+				fs.unlinkSync("./failedDownloads.initializeProject.json")
+			}
+		})
 	})
 })
