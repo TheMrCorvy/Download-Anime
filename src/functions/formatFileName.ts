e847d5b409fafbc943453dca340bf517e5201b62
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-20 11:00:30 +0900

    created format file names function

---
+++ src/functions/formatFileName.ts
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+import File from "../@types/file"
+
+const formatFileName: FormatFileName = ({ directory, customIndex, customName, url, id }) => {
+	const formatArr = url.split(".")
+	const format = formatArr[formatArr.length - 1] ? formatArr[formatArr.length - 1] : "mp4"
+	let i: string
+
+	if (customIndex) {
+		i = `${customIndex < 10 ? "0" + customIndex : customIndex}`
+	} else {
+		i = id
+	}
+
+	return capitalize(`${directory}/${i} - ${customName}.${format}`)
+}
+
+export const capitalize = (str: string): string => {
+	const evalCases = [" ", "-", "/"]
+
+	let result = str
+
+	evalCases.forEach((evalCase) => {
+		const arr = result.split(evalCase)
+
+		for (var i = 0; i < arr.length; i++) {
+			arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+		}
+
+		result = arr.join(evalCase)
+	})
+
+	return result
+}
+
+type FormatFileName = (props: File) => string
+
+export default formatFileName
