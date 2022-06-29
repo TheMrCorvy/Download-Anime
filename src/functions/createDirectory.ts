0099242a83b2db4549931d41cb977f5167c24f69
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-29 17:14:00 +0900

    added createDirectory function

--- src/functions/createDirectory.ts
+++ src/functions/createDirectory.ts
@@ -1,3 +1,38 @@
-const createDirectory = (dir: string): void => {}
+#!/usr/bin/env node
+
+import * as fs from "fs"
+
+import { capitalize } from "./formatFileName"
+import t from "./translate"
+
+import config from "../config"
+
+const createDirectory: CreateDirectory = (path) => {
+	const testing = process.env.NODE_ENV === "test"
+	const arrDir = capitalize(path).split("/")
+	let directoriesCreated = "./"
+
+	if (!fs.existsSync(config.mainDirectory)) {
+		fs.mkdirSync(config.mainDirectory)
+	}
+
+	directoriesCreated = directoriesCreated + config.mainDirectory
+
+	arrDir.forEach((dir) => {
+		if (!fs.existsSync(directoriesCreated + dir)) {
+			fs.mkdirSync(directoriesCreated + dir)
+
+			directoriesCreated = directoriesCreated + dir + "/"
+
+			!testing && console.log(`${t("folder")} ${dir} ${t("created")}`)
+		} else {
+			directoriesCreated = directoriesCreated + dir + "/"
+
+			!testing && console.log(`${t("folder")} ${dir} ${t("already_existed")}`)
+		}
+	})
+}
+
+type CreateDirectory = (path: string) => void
 
 export default createDirectory
