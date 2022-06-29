04798e5eb92bf32208e1a9d56d7cee0ebb6af814
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-29 17:13:47 +0900

    updated test

--- test/createDirectory.test.ts
+++ test/createDirectory.test.ts
@@ -3,37 +3,36 @@ import * as fs from "node:fs"
 import { expect } from "chai"
 
 import createDirectory from "../src/functions/createDirectory"
+import config from "../src/config"
 
 describe("the app should create directories only if needed", () => {
 	it("should create the directory if it doesn't exists", () => {
 		const dir = "test 1/testing-directories"
-		const dirCreated = "Test 1/Testing-Directories"
+		const dirCreated = config.mainDirectory + "Test 1/Testing-Directories"
 
 		createDirectory(dir)
 
 		expect(fs.existsSync(dirCreated)).to.be.true
 
 		after(() => {
-			if (fs.existsSync(dirCreated)) {
-				fs.unlinkSync(dirCreated)
-			}
+			fs.rmdirSync(dirCreated)
+			fs.rmdirSync(config.mainDirectory + "Test 1")
 		})
 	})
 
 	it("should reuse a directory that was already created, instead of creating a copy", () => {
-		const dir = "test 2/testing-directories"
-		const dirCreated = "Test 2/Testing-Directories"
+		const dir1 = "test 2"
+		const dir2 = "test 2/testing-directories"
+		const dirCreated = config.mainDirectory + "Test 2/Testing-Directories"
 
-		fs.mkdirSync("Test 2")
-
-		createDirectory(dir)
+		createDirectory(dir1)
+		createDirectory(dir2)
 
 		expect(fs.existsSync(dirCreated)).to.be.true
 
 		after(() => {
-			if (fs.existsSync(dirCreated)) {
-				fs.unlinkSync(dirCreated)
-			}
+			fs.rmdirSync(dirCreated)
+			fs.rmdirSync(config.mainDirectory + "Test 2")
 		})
 	})
 })
