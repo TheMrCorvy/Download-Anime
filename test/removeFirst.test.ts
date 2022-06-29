27aaa3bf042814fac7985eb7c55fbffd78b9f1ce
Gonzalo Salvador Corvalán <mr.corvy@gmail.com>
2022-06-29 18:54:02 +0900

    created test for removing first item of an array

---
+++ test/removeFirst.test.ts
@@ -0,0 +1,65 @@
+import { expect } from "chai"
+
+import File from "../src/@types/file"
+
+import removeFirst from "../src/functions/removeFirst"
+
+describe("the app should remove elements from arrays", () => {
+	it("should remove only the first item", () => {
+		const arr: File[] = [
+			{
+				id: "file 1",
+				customIndex: 0,
+				customName: "name",
+				directory: "directory",
+				url: "https://google.com",
+			},
+			{
+				id: "file 2",
+				customIndex: 1,
+				customName: "name",
+				directory: "directory",
+				url: "https://google.com",
+			},
+		]
+
+		const { firstElement, updatedArr } = removeFirst(arr)
+
+		expect(firstElement).to.be.eql(arr[0])
+		expect(updatedArr).to.be.eql([
+			{
+				id: "file 2",
+				customIndex: 1,
+				customName: "name",
+				directory: "directory",
+				url: "https://google.com",
+			},
+		])
+	})
+
+	it("should return empty arr if only has 1 item", () => {
+		const arr: File[] = [
+			{
+				id: "file 1",
+				customIndex: 0,
+				customName: "name",
+				directory: "directory",
+				url: "https://google.com",
+			},
+		]
+
+		const { firstElement, updatedArr } = removeFirst(arr)
+
+		expect(firstElement).to.be.eql(arr[0])
+		expect(updatedArr).to.be.eql([])
+	})
+
+	it("should check if there actually is anything to remove", () => {
+		const arr: File[] = []
+
+		const { firstElement, updatedArr } = removeFirst(arr)
+
+		expect(firstElement).to.be.eql(undefined)
+		expect(updatedArr).to.be.eql([])
+	})
+})
